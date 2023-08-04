3249bc33d875ecf4fd4dcace52b4b90bc45a4ebc
Simran Mandal <simranmandal0211@gmail.com>
2023-08-04 12:36:10 +0530

    create home.ejs as a view engine

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,3 +1,7 @@
 module.exports.home = function(request, respond){
-    return respond.end('<h1>Express is up for PlacementCell APP!</h1>');
+    // return respond.end('<h1>Express is up for PlacementCell APP!</h1>');
+
+    return respond.render('home', {
+        title: "home"
+    });
 }
