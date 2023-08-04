33bf80c84b1e0e6ad3c5174fa1570c9656576123
Simran Mandal <simranmandal0211@gmail.com>
2023-08-05 00:40:14 +0530

    Add express session and passport for authentication

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,50 +1,45 @@
 const passport = require('passport');
-const LocalStratergy = require('passport-local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
 
 // aurhentication using Passport
-passport.use(new LocalStratergy({
+passport.use(new LocalStrategy({
         usernameField: 'email'
     },
-    function(email, password, done){
-        // done- callback function reporting back to passport.js
-
-        // find a user and establishe the identity
-        User.findOne({email: email}, function(err, user){
-            if(err){
-                console.log('Error in finding user -> Passport');
-                return done(err);
+    async function(email, password){
+        try{
+            let user = await User.findOne({email: email});
+            if (!user || user.password != password){
+                return false;
+                // null- no error  and  false- authentication
             }
-            if(!user || user.password != password){
-                console.log('Invalid Username/Password');
-                return done(null, false);
-                // null- no error
-                //  false- authentication
-            }
-        return done(null, user);
-        });
+            return user;
+        }catch(err){
+            console.log('Error in finding user -> Passport', err);
+            throw err;
+        }
     }
 ));
 
 // serialize- the user to decide which key is to be kept in the cookies 
 // serializeUser- inbuild function
 
-        // (we find id --> sended to cookie --> browser)
-passport.serializeUser(function(user,done){
-    done(null, user.id);
+// (we find id --> sended to cookie --> browser)
+passport.serializeUser(function(user) {
+    return user.id;
 });
 
-        // (browser make request --> deserialize --> find user again)
+// (browser make request --> deserialize --> find user again)
 // deserializing the user from the key in the cookies
-passport.deserializeUser(function(id, done){
-    User.findyId(id, function(err, user){
-        if(err){
-            console.log('Error in finding uer --> Passport');
-            return done(err);
-        }
-        return done(null, user);    //null- no error, user- user found
-    })
+passport.deserializeUser(async function(id){
+    try{
+        let user = await User.findById(id).exec();
+        return user;
+    }catch (err) {
+        console.log('Error in finding user -> Passport', err);
+        throw err;
+    }
 });
 
 module.exports = passport;
--- index.js
+++ index.js
@@ -15,7 +15,13 @@ app.use(express.static('./assets'));
 app.use(expressLayouts);
 
 // setup database
-const db = require('./config/mongoose')
+const db = require('./config/mongoose');
+
+// use for session cookie
+const session = require('express-session');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+
 app.use(express.urlencoded()); //add body-parser
 
 // cookie
@@ -34,6 +40,22 @@ app.set('layout extractScripts', true);
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+
+app.use(session({
+    name: 'placementcellapp', 
+    //Todo change the secret before deployment in production mode
+    secret: 'blashsomething',
+    saveUninitialized: false, 
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 
 // use express router
