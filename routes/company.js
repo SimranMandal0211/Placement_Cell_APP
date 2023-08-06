d5001a860a70e858e607a02b2285e651fbc6267e
Simran Mandal <simranmandal0211@gmail.com>
2023-08-06 16:20:51 +0530

    check authentication on allocate, schedule and upate interviews in company

--- routes/company.js
+++ routes/company.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const companyController = require('../controller/company_controller');
 
-router.get('/home', companyController.companyPage);
-router.get('/allocate', companyController.allocateInterview);
+router.get('/home',passport.checkAuthentication,  companyController.companyPage);
+router.get('/allocate', passport.checkAuthentication, companyController.allocateInterview);
 
-router.post('/schedule-interview', companyController.scheduleInterview);
-router.post('/update-status/:id', companyController.updateStatus);
+router.post('/schedule-interview', passport.checkAuthentication, companyController.scheduleInterview);
+router.post('/update-status/:id', passport.checkAuthentication, companyController.updateStatus);
 
 module.exports = router;
