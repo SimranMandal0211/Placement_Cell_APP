66604e4df7903426448beeaaf86ddfb6280dc2aa
Simran Mandal <simranmandal0211@gmail.com>
2023-09-16 15:03:23 +0530

    mongodb connect

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,24 +2,25 @@ const mongoose = require('mongoose');
 const env = require('../config/environment');
 
 // local mongodb connection
-// mongoose.connect(`mongodb://0.0.0.0/${env.db}`);
+mongoose.connect(env.db);
 
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "Error connecting to mongodb"));
 
-// db.once('open', function(){
-//     console.log('connected to database:: MongoDB');
-// });
+db.once('open', function(){
+    console.log('connected to database:: MongoDB');
+});
 
-// module.exports = db;
+module.exports = db;
 
 
 // 2.
-module.exports.db = async () => {
-    try{
-      await mongoose.connect(env.db)
-      console.log('connect to MongoDB sucessfully')
-    }catch(error){
-      console.timeLog('connect failed' +error.message)
-    }
-  }
+// module.exports.db = async () => {
+//     try{
+//       console.log('db string',env.db);
+//       await mongoose.connect(env.db)
+//       console.log('connect to MongoDB sucessfully')
+//     }catch(error){
+//       console.timeLog('connect failed' +error.message)
+//     }
+//   }
