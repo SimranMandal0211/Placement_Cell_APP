2f27b3e39294ae2621d867aca6679c01a9907129
Simran Mandal <simranmandal0211@gmail.com>
2023-09-18 16:54:36 +0530

    update mongodb version

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,35 +1,23 @@
 const mongoose = require('mongoose');
 const env = require('../config/environment');
 
-module.exports.db = async () => {
-    try{
-      await mongoose.connect(env.db)
-      console.log('connect to MongoDB sucessfully')
-    }catch(error){
-      console.timeLog('connect failed' +error.message)
-    }
-  }
-
-// local mongodb connection
-// mongoose.connect(env.db);
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, "Error connecting to mongodb"));
-
-// db.once('open', function(){
-//     console.log('connected to database:: MongoDB');
-// });
-
-// module.exports = db;
-
-
-// 2.
 // module.exports.db = async () => {
 //     try{
-//       console.log('db string',env.db);
 //       await mongoose.connect(env.db)
 //       console.log('connect to MongoDB sucessfully')
 //     }catch(error){
 //       console.timeLog('connect failed' +error.message)
 //     }
 //   }
+
+// local mongodb connection
+mongoose.connect(env.db);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "Error connecting to mongodb"));
+
+db.once('open', function(){
+    console.log('connected to database:: MongoDB');
+});
+
+module.exports = db;
